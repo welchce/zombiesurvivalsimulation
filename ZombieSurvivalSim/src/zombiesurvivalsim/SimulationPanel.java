e48632fd59ff180779f7343a2ab29af5855cfdca
Rj.Cox101 <Rj.Cox101@06d20c9e-b9b0-11de-8784-f7ad4773dbab>
2009-11-06 21:17:51 +0000

    Added some sweet images to the UI, and demonstrated how drawing ingame will work

--- ZombieSurvivalSim/src/zombiesurvivalsim/SimulationPanel.java
+++ ZombieSurvivalSim/src/zombiesurvivalsim/SimulationPanel.java
@@ -7,17 +7,36 @@ package zombiesurvivalsim;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.Image;
+import javax.imageio.ImageIO;
 /**
  *
  * @author Raymond Cox <rj.cox101 at gmail.com>
  */
 public class SimulationPanel extends JPanel {
+    Image _kennyImage, _zombieImage;
+
     public SimulationPanel() {
+        try {
+            _kennyImage = ImageIO.read(this.getClass().getResource("images/Human.gif"));
+            _zombieImage = ImageIO.read(this.getClass().getResource("images/Zombie.gif"));
+        } catch (java.lang.Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
     @Override
     public void paint(Graphics g) {
-        g.setColor(Color.black);
+        g.setColor(Color.DARK_GRAY);
         g.fillRect(0, 0, getWidth(), getHeight());
+        // Fun with zombies
+        g.drawImage(_zombieImage, getWidth()/2, getWidth()/2+24, this);
+        g.drawImage(_kennyImage, getWidth()/2, getWidth()/2, this);
+        g.drawImage(_zombieImage, getWidth()/2+24, getWidth()/2, this);
+        g.drawImage(_zombieImage, getWidth()/2+24, getWidth()/2+24, this);
+        g.drawImage(_zombieImage, getWidth()/2-24, getWidth()/2+24, this);
+        g.drawImage(_zombieImage, getWidth()/2-24, getWidth()/2, this);
+        g.drawImage(_zombieImage, getWidth()/2-24, getWidth()/2-24, this);
+        g.drawImage(_zombieImage, getWidth()/2, getWidth()/2-24, this);
+        g.drawImage(_zombieImage, getWidth()/2+24, getWidth()/2-24, this);
     }
 }
