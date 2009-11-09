5b9859f5cdc6e276b8fe9d5e754c0e25fd1aee60
welchce <welchce@06d20c9e-b9b0-11de-8784-f7ad4773dbab>
2009-11-09 21:20:03 +0000

    Suck it bitches

--- trunk/ZombieSurvivalSim/src/zombiesurvivalsim/Logger.java
+++ trunk/ZombieSurvivalSim/src/zombiesurvivalsim/Logger.java
@@ -32,25 +32,36 @@ public class Logger {
 
     /*
      * The default constructor for the Logger class, creates a log file.
+     *  @pre computer has valid date
+     *  @post _fileName as been created
     */
     public Logger() {
         final String DATE_FORMAT_NOW = "MM-dd-yy HH.mm.ss";
         Date curTime = Calendar.getInstance().getTime();
         _fileName = new SimpleDateFormat(DATE_FORMAT_NOW).format(curTime)+".log";
     }
+
     /*
      * Constructor that takes a filename as a parameter.
+     *  @pre file name doesn't already exist
+     *  @post _fileName as been created
     */
     public Logger(String fileName) {
         _fileName = fileName;
     }
+
     /*
      * returns the filename of the log file.
+     *  @pre log exists
+     *  @post _fileName is returned
     */
     public String getFileName() { return _fileName; }
 
     /*
      * writes the passed String st into the log file.
+     *  @pre log file is valid
+     *  @pre event is valid string
+     *  @post log file contains new event
     */
     public void write(Event thisEvent) throws java.io.IOException {
         FileWriter outFile = new FileWriter(_fileName,true);
